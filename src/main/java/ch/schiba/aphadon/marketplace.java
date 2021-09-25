7a889f13af691f3b68f83bafdbace8b93943e462
Jann Erhardt <59923683+FPGSchiba@users.noreply.github.com>
2021-09-25 18:36:09 +0200

    Initiated Project

---
+++ src/main/java/ch/schiba/aphadon/marketplace.java
@@ -0,0 +1,17 @@
+package ch.schiba.aphadon;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class marketplace extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
